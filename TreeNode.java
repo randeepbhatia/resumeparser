02d7afe0d32dab34342108e32445cd34336111e4
raghav123 <raghav123@berkeley.edu>
2014-06-08 11:58:11 -0700

    Added toString method

--- TreeNode.java
+++ TreeNode.java
@@ -26,6 +26,10 @@ public class TreeNode implements Iterable<TreeNode> {
 	  public boolean removeChild(TreeNode n) {
 	    return children.remove(n);
 	  }
+	  
+	  public Set<TreeNode> getChildren() {
+		  return children;
+	  }
 
 	  public Iterator<TreeNode> iterator() {
 	    return children.iterator();
@@ -34,6 +38,47 @@ public class TreeNode implements Iterable<TreeNode> {
 	  public static void main(String[] argv) {
 		  TreeNode node1 = new TreeNode("Test");
 		  System.out.println(node1.getName());
+	  }
+	  
+	  public String toString() {
+		  	String returnString = "";
+		  	int level = 0;
+			Set<Set<TreeNode>> setOfChildrenSets = new HashSet<Set<TreeNode>>(); //List of all children at that level
+			Set<TreeNode> tempchildren = new HashSet<TreeNode>(); 
+			tempchildren.add(this); //0th level child is just root
+			setOfChildrenSets.add(tempchildren); //Just the root for now
+			
+			boolean thingsLeft = true;
+			
+			while (thingsLeft) {
+				
+				thingsLeft = false;
+				
+				for (Set<TreeNode> children : setOfChildrenSets) { //Goes through all children sets at that level
+				
+					for (TreeNode child : children) { //Goes through each child in each set
+						
+						thingsLeft = true;
+						returnString = returnString + child.getName() + " " + level + "\n";
+						
+					}
+				}
+				
+				level++;
+				Set<Set<TreeNode>> newSetOfChildrenSets = new HashSet<Set<TreeNode>>();
+				for (Set<TreeNode> children: setOfChildrenSets) { //Goes through all children sets at current level
+				
+					for (TreeNode child: children) {
+					
+						newSetOfChildrenSets.add(child.getChildren()); //Adds children of every child for that level
+					
+					}
+				
+				}
+				setOfChildrenSets = newSetOfChildrenSets;
+				
+			}
+			return returnString;
 	  }
 	  
 }
